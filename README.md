c4b3eb5acd615fffb7f93d4b6d9ec8aba5366dc6
Danny <dannygeisz@berkeley.edu>
2024-09-16 07:01:36 -0700

    Init Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Chai Dojo
+
+Doing interpretability analysis on [Chai-1](https://www.chaidiscovery.com/blog/introducing-chai-1)
---
+++ danny_setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git config --global user.email "dannygeisz@berkeley.edu"
+git config --global user.name "Danny"
+git config --global credential.helper store
