55481d028a4d8f2870f890f7733152c0be147be6
Danny <dannygeisz@berkeley.edu>
2024-10-04 22:57:25 +0000

    EXP: Last experiment worked quite well, we're going to add aux loss

--- notebooks/test_osae.py
+++ notebooks/test_osae.py
@@ -1,6 +1,6 @@
 # %%
-%load_ext autoreload
-%autoreload 2
+# %load_ext autoreload
+# %autoreload 2
 
 # %%
 import matplotlib.pyplot as plt
@@ -15,7 +15,6 @@ from chai_lab.interp.experiment_tracker import ExperimentTracker
 from chai_lab.interp.o_sae import OSae
 from chai_lab.interp.train import OSAETrainer
 from chai_lab.interp.s3 import s3_client
-
 
 
 # %%
@@ -40,18 +39,16 @@ cfg = OSAEConfig(
     lr=1e-3,
     beta1=0.9,
     beta2=0.999,
-    # aux_fraction=1/64,
-    aux_fraction=0,
+    aux_fraction=1 / 32,
+    # aux_fraction=0,
     subtract_mean=True,
-
     use_scheduler=True,
     num_batches_before_increase=1000,
     increase_interval=500,
-    final_multiplier=20.,
-
+    final_multiplier=20.0,
     use_decay=False,
     decay_rate=0.997,
-    final_rate=1e-3
+    final_rate=1e-3,
     # aux_fraction=None
 )
 
@@ -62,170 +59,149 @@ trainer = OSAETrainer(cfg, s3=s3_client)
 
 run = tracker.new_experiment(
     "osae-real-acts",
-    "The lower sneak attack worked, we're going to try to juice it to lr=2e-3 (and with no aux loss)",
+    "Last experiment worked quite well, we're going to add aux loss",
     config=asdict(cfg),
 )
 
 
 # %%
 trainer.train(6000, run)
-trainer.osae.save_model_to_aws(s3_client, f"osae_v1_1EN3_2EN2_{32 * 2048}.pth")
+trainer.osae.save_model_to_aws(s3_client, f"osae_v1_1EN3_2EN2_aux_{32 * 2048}.pth")
 tracker.finish()
 
 
+# # %%
+# trainer.osae.save_model_to_aws(s3_client, f"osae_v1_1EN3_1EN2_{32 * 2048}.pth")
 
+# # %%
+# tracker.finish()
 
-# %%
-trainer.osae.save_model_to_aws(s3_client, f"osae_v1_1EN3_1EN2_{32 * 2048}.pth")
 
-# %%
-tracker.finish()
+# # %%
+# new_osae = OSae(cfg, dtype=torch.bfloat16)
+# new_osae.load_model_from_aws(s3_client, "osae_test.pth")
 
+# # %%
+# # Check if the encoder data is the same
+# (trainer.osae.encoder == new_osae.encoder).all()
 
 
+# # trainer.train(1000, run)
 
-# %%
-new_osae = OSae(cfg, dtype=torch.bfloat16)
-new_osae.load_model_from_aws(s3_client, "osae_test.pth")
+# # %%
 
-# %%
-# Check if the encoder data is the same
-(trainer.osae.encoder == new_osae.encoder).all()
+# run.finish()
 
+# # %%
+# feature_counts = trainer.get_feature_counts_for_batches(10)
 
+# # %%
+# # Filter feature_counts so we only
 
+# plt.hist(feature_counts.detach().cpu().numpy(), bins=64, log=False, range=(0, 200))
+# plt.show()
 
+# # %%
+# import torch
 
+# bins = torch.bincount(feature_counts)
 
+# # %%
+# plt.plot(bins[:40].detach().cpu().numpy())
 
-# trainer.train(1000, run)
 
-# %%
+# # feature_counts.argmax()
 
-run.finish()
+# # %%
+# (feature_counts > 3000 ).sum()
 
-# %%
-feature_counts = trainer.get_feature_counts_for_batches(10)
+# # %%
+# feature_counts.sum() / 32
 
-# %%
-# Filter feature_counts so we only
+# # %%
+# feature_counts.float().std()
 
-plt.hist(feature_counts.detach().cpu().numpy(), bins=64, log=False, range=(0, 200))
-plt.show()
 
-# %%
-import torch
+# # %%
 
-bins = torch.bincount(feature_counts)
+# plt.plot(trainer.loss_per_batch[100:])
+# plt.show()
 
-# %%
-plt.plot(bins[:40].detach().cpu().numpy())
+# # %%
 
+# x, y = trainer.num_dead_per_batch
+# plt.plot(x, y)
 
-# feature_counts.argmax()
+# # %%
 
-# %%
-(feature_counts > 3000 ).sum()
+# # %%
+# # train
 
-# %%
-feature_counts.sum() / 32
+# # %%
+# import einops
+# import torch
 
-# %%
-feature_counts.float().std()
+# x = trainer.data_loader.next_batch().to(trainer.osae.device)
+# osae = trainer.osae
 
 
+# x -= osae.b_dec
 
+# acts = einops.einsum(
+#     x, osae.encoder, "batch d_model, k d_group d_model -> batch k d_group"
+# )
 
+# acts += osae.b_enc
 
+# out, _max_values, max_indices = osae.decode_from_acts(acts)
 
+# out += osae.b_dec
 
+# e = (x - out).pow(2).sum()
+# total_variance = (x - x.mean(0)).pow(2).sum()
 
+# fvu = e / total_variance
 
-# %%
+# feature_counts = osae.get_feature_counts(max_indices)
 
-plt.plot(trainer.loss_per_batch[100:])
-plt.show()
+# # %%
 
-# %%
 
-x, y = trainer.num_dead_per_batch
-plt.plot(x, y)
+# dead_mask = trainer.dead_mask
 
-# %%
+# total_feature_counts = trainer.total_feature_counts
 
-# %%
-# train
+# # %%
+# dead_mask.any(dim=-1).all()
 
-# %%
-import einops
-import torch
+# # %%
 
-x = trainer.data_loader.next_batch().to(trainer.osae.device)
-osae = trainer.osae
 
+# if osae.cfg.aux_fraction and dead_mask is not None and dead_mask.any():
+#     aux_acts = torch.where(dead_mask[None], acts, -torch.inf)
+#     aux_out, _, _ = osae.decode_from_acts(aux_acts)
 
+#     e_hat = (aux_out - x).pow(2).sum()
+#     aux_fvu = e_hat / total_variance
+# else:
+#     aux_fvu = torch.tensor(0.0, device=osae.device)
 
-x -= osae.b_dec
+# # %%
+# # Check if dead_mask has any True
+# dead_mask.any()
 
-acts = einops.einsum(
-    x, osae.encoder, "batch d_model, k d_group d_model -> batch k d_group"
-)
 
-acts += osae.b_enc
+# # %%
+# trainer.train(1000)
 
-out, _max_values, max_indices = osae.decode_from_acts(acts)
 
-out += osae.b_dec
+# # %%
 
-e = (x - out).pow(2).sum()
-total_variance = (x - x.mean(0)).pow(2).sum()
+# plt.plot(trainer.loss_per_batch[:100])
+# plt.show()
 
-fvu = e / total_variance
+# # %%
+# plt.plot(trainer.num_dead_per_batch[1])
 
-feature_counts = osae.get_feature_counts(max_indices)
-
-# %%
-
-
-dead_mask = trainer.dead_mask
-
-total_feature_counts = trainer.total_feature_counts
-
-# %%
-dead_mask.any(dim=-1).all()
-
-# %%
-
-
-
-if osae.cfg.aux_fraction and dead_mask is not None and dead_mask.any():
-    aux_acts = torch.where(dead_mask[None], acts, -torch.inf)
-    aux_out, _, _ = osae.decode_from_acts(aux_acts)
-
-    e_hat = (aux_out - x).pow(2).sum()
-    aux_fvu = e_hat / total_variance
-else:
-    aux_fvu = torch.tensor(0.0, device=osae.device)
-
-# %%
-# Check if dead_mask has any True
-dead_mask.any()
-    
-
-
-# %%
-trainer.train(1000)
-
-
-# %%
-
-plt.plot(trainer.loss_per_batch[:100])
-plt.show()
-
-# %%
-plt.plot(trainer.num_dead_per_batch[1])
-
-# %%
-trainer.num_dead_per_batch[]
-
-
+# # %%
+# trainer.num_dead_per_batch[]
